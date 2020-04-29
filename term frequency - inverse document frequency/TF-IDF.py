bf6aec8b8c6c4437a1141c9b3cf601a154fa5850
khoanguyenvietmanh <57497097+khoanguyenvietmanh@users.noreply.github.com>
2020-04-29 20:43:09 +0600

    renew

--- term frequency - inverse document frequency/TF-IDF.py
+++ term frequency - inverse document frequency/TF-IDF.py
@@ -25,21 +25,22 @@ def collect_data_from(parent_dir,newsgroups_list):
 
     for group_id, newsgroup in enumerate(newsgroups_list):
         label = group_id
-        dir_path = parent_dir  + newsgroup + '/'
+        print(label)
+        dir_path = parent_dir + newsgroup + '/'
         files = [(filename,dir_path + filename) for filename in os.listdir(dir_path) if os.path.isfile(dir_path + filename)]
         files.sort()
         #print(files)
-    for filename,filepath in files:
-        with open(filepath,"r") as f:
-            text = f.read().upper()
-        #remove stop words then stem remaining words
-        words = [stemmer.stem(word) for word in re.split('\W+',text) if word not in stop_words]
-        #print(words)
-        #combine remaining words
-        content = " ".join(words)
-        assert len(content.splitlines()) == 1
-        data.append(str(label) + '<fff>' + filename + '<fff>' + content)
-        #print(data)
+        for filename,filepath in files:
+            with open(filepath,"r") as f:
+                text = f.read().lower()
+                #remove stop words then stem remaining words
+                words = [stemmer.stem(word) for word in re.split('\W+',text) if word not in stop_words]
+                #print(words)
+                #combine remaining words
+                content = " ".join(words)
+                assert len(content.splitlines()) == 1
+                data.append(str(label) + '<fff>' + filename + '<fff>' + content)
+    #print(data)
     return data
 def compute_idf(df,corpus_size):
     assert df > 0
@@ -60,14 +61,14 @@ def generate_vocabulary(data_path):
         for word in words:
             doc_count[word]+=1
 
-        words_idfs = [(word,compute_idf(document_freq,corpus_size)) for word,document_freq in zip(doc_count.keys(),doc_count.values()) if document_freq>10 and not word.isdigit()]
+    words_idfs = [(word,compute_idf(document_freq,corpus_size)) for word,document_freq in zip(doc_count.keys(),doc_count.values()) if document_freq>10 and not word.isdigit()]
 
-        words_idfs.sort(key = lambda abc:abc[1],reverse = True)
+    words_idfs.sort(key = lambda abc:abc[1],reverse = True)
 
-        #print(f"Vocabulary size:{len(words_idfs)}")
+    print(f"Vocabulary size:{len(words_idfs)}")
 
-        with open('../20news-bydate/words_idfs.txt', 'w') as f:
-            f.write("\n".join([words + "<fff>" + str(idf) for words,idf in words_idfs]))
+    with open('../20news-bydate/words_idfs.txt', 'w') as f:
+        f.write("\n".join([words + "<fff>" + str(idf) for words,idf in words_idfs]))
 def get_tf_idf(data_path):
     #pre-computed idf values
     with open('../20news-bydate/words_idfs.txt', 'r') as f:
@@ -75,49 +76,51 @@ def get_tf_idf(data_path):
 
         word_IDs = dict([(word,index) for index , (word, idf) in enumerate(words_idfs)])
 
+        #print(word_IDs)
+
         idfs = dict(words_idfs)
 
-        with open(data_path) as f:
-            documents = [(int(line.split("<fff>")[0]),int(line.split("<fff>")[1]),line.split("<fff>")[2]) for line in f.read().splitlines()]
+    with open(data_path) as f:
+        documents = [(int(line.split("<fff>")[0]),int(line.split("<fff>")[1]),line.split("<fff>")[2]) for line in f.read().splitlines()]
 
-        data_tf_idf = []
-        for document in documents:
-            label,doc_id,text = document
-            words  = [word for word in text.split() if word in idfs]
+    data_tf_idf = []
+    for document in documents:
+        label,doc_id,text = document
+        words  = [word for word in text.split() if word in idfs]
 
-            print(words)
+        word_set = list(set(words))
 
-            word_set = list(set(words))
+        max_term_freq = max([words.count(word) for word in word_set])
 
-            max_term_freq = max([words.count(word) for word in word_set])
+        words_tfidfs = []
 
-            words_tfidfs = []
+        sum_squares = 0.0
 
-            sum_squares = 0.0
+        for word in word_set:
+            term_freq = words.count(word)
+            tf_idf_value = term_freq * 1./ max_term_freq * idfs[word]
+            words_tfidfs.append((word_IDs[word],tf_idf_value))
+            sum_squares += tf_idf_value**2
 
-            for word in word_set:
-                term_freq = words.count(word)
-                tf_idf_value = term_freq * 1./ max_term_freq * idfs[word]
-                words_tfidfs.append((word_IDs[word],tf_idf_value))
-                sum_squares += tf_idf_value**2
+        words_tfidfs_normalized = [str(index) + ":" + str(tf_idf_value/np.sqrt(sum_squares)) for index,tf_idf_value in words_tfidfs]
 
-            words_tfidfs_normalized = [str(index) + ":" + str(tf_idf_value/np.sqrt(sum_squares)) for index,tf_idf_value in words_tfidfs]
+        sparse_rep = ' '.join(words_tfidfs_normalized)
 
-            sparse_rep = ' '.join(words_tfidfs_normalized)
+        data_tf_idf.append((label,doc_id,sparse_rep))
 
-            data_tf_idf.append((label,doc_id,sparse_rep))
-
-            with open('../20news-bydate/data_tf_idf.txt', 'w') as f:
-                f.write("\n".join([str(label) + "<fff>" + str(doc_id) + "<fff>" + str(sparse_rep) for label,doc_id,sparse_rep in data_tf_idf]))
+    with open('../20news-bydate/data_tf_idf.txt', 'w') as f:
+        f.write("\n".join([str(label) + "<fff>" + str(doc_id) + "<fff>" + str(sparse_rep) for label,doc_id,sparse_rep in data_tf_idf]))
 
 list_newsgroups,train_dir,test_dir = gather_20newsgroups_data()
 
-#print(list_newsgroups)
 #train_data = collect_data_from(train_dir,list_newsgroups)
 
 #test_data  = collect_data_from(test_dir,list_newsgroups)
 
-#generate_vocabulary('../20news-bydate/20news-train-processed.txt')
+#with open('../20news-bydate/20news-train-processed.txt',"w") as f:
+#    f.write("\n".join(train_data))
+#with open('../20news-bydate/20news-test-processed.txt',"w") as f:
+#    f.write("\n".join(test_data))
+generate_vocabulary('../20news-bydate/20news-train-processed.txt')
 
 get_tf_idf('../20news-bydate/20news-train-processed.txt')
-
